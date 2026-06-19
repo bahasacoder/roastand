a95a0fe610a4e214f9a204e90de36180d1ee8650
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-06-19 21:27:55 +0800

    Simplify className in layout.tsx
    Removed font classes from the HTML tag.

--- app/landing-page-templates/layout.tsx
+++ app/landing-page-templates/layout.tsx
@@ -13,7 +13,7 @@ export default function LPTemplatesLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
           {children}
