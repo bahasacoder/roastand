6d4c976a1ee76a94bd5e67c8aec0a568b0e0f3dd
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-06-19 22:07:07 +0800

    Add Navbar Menu heading to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <h1>Navbar Menu Menu</h1>
           {children}
       </body>
     </html>
