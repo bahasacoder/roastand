02d7f1ebe769476b1475c42bc422074ae5c9b044
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-06-22 18:48:46 +0800

    Delete lib directory

--- lib/octokit.ts
+++
@@ -1,8 +0,0 @@
-import { Octokit } from "@octokit/rest";
-import { createTokenAuth } from "@octokit/auth-token";
-
-const octokit = new Octokit({
-  auth: process.env.GITHUB_TOKEN,
-});
-
-export default octokit;
