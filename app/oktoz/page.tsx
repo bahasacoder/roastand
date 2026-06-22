85722e1b12ca143cea0c774487246dded841e025
bahasacoder <98062799+bahasacoder@users.noreply.github.com>
2026-06-22 20:25:07 +0800

    Remove unused API call in oktozPage function
    Removed unused API call to fetch repository content.

--- app/oktoz/page.tsx
+++ app/oktoz/page.tsx
@@ -5,17 +5,6 @@ const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 export default async function oktozPage() {
 
 
-  const response = await octokit.rest.repos.getContent({
-  mediaType: {
-    format: "raw",
-  },
-  owner: "bahasacoder",
-  repo: "roastand",
-  path: "db/items.json",
-});
-  //, JSON.parse(data).title
-console.log("package title: %s");
-  
  return (
    <>
      <div>Oktoz Page</div>
